429278c2acb88dfd8aedddcce27e0e7c4c334846
Viet-Anh Nguyen <vietanh@vietanhdev.com>
2018-05-17 15:49:31 +0700

    remove sensitive data

---
+++ server/controllers/collection.js
@@ -0,0 +1,14 @@
+// Load required packages
+var CollectionModel = require('../models/collection');
+
+var CollectionModel = {};
+
+// Load language file
+var getText = require('../error_msg_vi.json');
+
+
+CollectionModel.login = function(req, res) {
+
+}
+
+module.exports = CollectionModel;
