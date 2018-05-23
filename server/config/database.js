c4c2e96e77618f9949a40496ab5b0eef663fb4ad
Viet-Anh Nguyen <vietanh@vietanhdev.com>
2018-05-23 11:32:18 +0700

    update database config

--- server/config/database.js
+++ server/config/database.js
@@ -4,7 +4,7 @@ const dbConfig  = {
     password: "156432",
     database: "ictflashcard",
     port     : 3306,
-    acquireTimeout: 10000
+    acquireTimeout: 10000 
 };
 
 module.exports = dbConfig;
