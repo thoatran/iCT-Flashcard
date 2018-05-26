f65eecff8434b25e9fbd351afce3655ae334d0d2
Viet-Anh Nguyen <vietanh@vietanhdev.com>
2018-05-26 17:19:10 +0700

    save recent words in dictionary

--- server/public/vi/assets/js/dictionary-page.js
+++ server/public/vi/assets/js/dictionary-page.js
@@ -1,49 +1,97 @@
+ // Overrides the default autocomplete filter function to search only from the beginning of the string
+ $.ui.autocomplete.filter = function (array, term) {
+    var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(term), "i");
+    return $.grep(array, function (value) {
+        return matcher.test(value.label || value.value || value);
+    });
+};
+
+function findWord(word) {
+
+    // hide suggestions
+    $(".ui-menu-item").hide();
+
+    // Load word
+    $.ajax({
+        type: 'GET',
+        url: '/api/dictionary/ev?lookup=' + encodeURIComponent(word),
+        contentType: 'application/json; charset=utf-8',
+        cache: false,
+        success: function(data) {
+            console.log(data);
+            if (data.success != true) {
+                $( "#meaning-wrapper" ).html("Không tìm thấy từ bạn yêu cầu.");
+            } else {
+                // Load Quick Search
+                $( "#meaning-wrapper" ).html(`
+                <h3 style="display:inline" class="word text-primary border-bottom border-gray">`+data.word+`</h3>
+                <span style="display:inline" class="ipapron text-secondary">`+data.ipapron+`</span>
+                <!--<button style="display:inline;" type="button" class="btn btn-outline-dark btn-sm">
+                    <i class="fa fa-plus-square-o" aria-hidden="true"></i>
+                    Tạo flashcard
+                </button>-->
+                <div>
+                    <button class="btn btn-light" onclick='responsiveVoice.speak("`+data.word+`", "UK English Female");' >🔊 UK</button>
+                    <button class="btn btn-light" onclick='responsiveVoice.speak("`+data.word+`", "US English Female");' >🔊 US</button>
+                </div>
+                <p class="meaning"><pre>`+data.meaning+`</pre></p>
+                `);
+            }
+
+        }, error: function(xhr) {
+            $( "#meaning-wrapper" ).html("Không tìm thấy từ bạn yêu cầu.");
+        }});
+
+        updateRecentWordList(word);
+
+}
+
+var updateRecentWordList = function(word) {
+
+    // Save word to recently list and show up the list
+    if (typeof localStorage !== 'undefined') {
+        let recentWordList = [];
+        if (localStorage.dictRecentList) {
+            recentWordList = JSON.parse(localStorage.dictRecentList);
+        }
+
+        if (typeof word != "undefined") {
+            recentWordList.push(word);
+        }
+
+        // Remove duplicated values
+        let recentWordList_unduplicated = [];
+        $.each(recentWordList, function(i, el){
+            if($.inArray(el, recentWordList_unduplicated) === -1) recentWordList_unduplicated.push(el);
+        });
+        recentWordList = recentWordList_unduplicated;
+
+        // Only keep 8 words in recent list
+        while (recentWordList.length > 8) {
+            recentWordList.shift();
+        }
+
+        localStorage.setItem("dictRecentList", JSON.stringify(recentWordList))
+    
+        // Show recent words
+        if (recentWordList.length > 0) {
+            let wordListBtns = "<h6 class='m-1'>Các từ vừa tra</h6>";
+            for (let i = 0; i < recentWordList.length; i++) {
+                wordListBtns += `<button class="btn btn-light m-1" onclick="findWord('`+recentWordList[i]+`')">`+recentWordList[i]+`</button>`;
+            }
+
+            $("#recent-word-list").html(wordListBtns);
+        }
+        
+    
+    }
+}
+
+
 $(document).ready(function() {
 
-    // Overrides the default autocomplete filter function to search only from the beginning of the string
-    $.ui.autocomplete.filter = function (array, term) {
-        var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(term), "i");
-        return $.grep(array, function (value) {
-            return matcher.test(value.label || value.value || value);
-        });
-    };
-
-    function findWord(word) {
-
-        // hide suggestions
-        $(".ui-menu-item").hide();
-
-        console.log("Searching: " + word);
-        $.ajax({
-            type: 'GET',
-            url: '/api/dictionary/ev?lookup=' + encodeURIComponent(word),
-            contentType: 'application/json; charset=utf-8',
-            cache: false,
-            success: function(data) {
-                console.log(data);
-                if (data.success != true) {
-                    $( "#meaning-wrapper" ).html("Không tìm thấy từ bạn yêu cầu.");
-                } else {
-                    // Load Quick Search
-                    $( "#meaning-wrapper" ).html(`
-                    <h3 style="display:inline" class="word text-primary border-bottom border-gray">`+data.word+`</h3>
-                    <span style="display:inline" class="ipapron text-secondary">`+data.ipapron+`</span>
-                    <!--<button style="display:inline;" type="button" class="btn btn-outline-dark btn-sm">
-                        <i class="fa fa-plus-square-o" aria-hidden="true"></i>
-                        Tạo flashcard
-                    </button>-->
-                    <div>
-                        <button class="btn btn-light" onclick='responsiveVoice.speak("`+data.word+`", "UK English Female");' >🔊 UK</button>
-                        <button class="btn btn-light" onclick='responsiveVoice.speak("`+data.word+`", "US English Female");' >🔊 US</button>
-                    </div>
-                    <p class="meaning"><pre>`+data.meaning+`</pre></p>
-                    `);
-                }
-
-            }, error: function(xhr) {
-                $( "#meaning-wrapper" ).html("Không tìm thấy từ bạn yêu cầu.");
-            }});
-    }
+    // Show the recent word list
+    updateRecentWordList();
 
     // Set event for Search button
     $('#btn-lookup').on('click', function(){
